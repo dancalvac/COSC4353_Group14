51fa8751822efdff107e09002646b4a5da168915
Charity Smith <charitilove257@gmail.com>
2025-07-03 19:32:00 -0500

    finished admin page with added sample volunteers

--- client/src/components/AdminMatching.jsx
+++ client/src/components/AdminMatching.jsx
@@ -12,12 +12,14 @@ function AdminMatching() {
         const sampleVolunteers = [
             { id: 1, name: "Charity Smith", skills: "First Aid, ASL" },
             { id: 2, name: "Henry Nguyen",   skills: "Logistics, Driving" },
-            { id: 3, name: "Max Gibson",    skills: "Photography, Bilingual" }
+            { id: 3, name: "Max Gibson",    skills: "Photography, Bilingual" },
+            { id: 4, name: "Daniel Calvac", skills: "Dogsitter, Carpenter"} 
         ];
         const sampleEvents = [
-            { id: 1, name: "Community Cleanup", date: "2025-07-15" },
-            { id: 2, name: "Fundraising Gala",  date: "2025-08-01" },
-            { id: 3, name: "Food Drive", date: "2025-09-10" }
+            { id: 1, name: "Community Cleanup", date: "07-15-2025" },
+            { id: 2, name: "Fundraising Gala",  date: "08-01-2025" },
+            { id: 3, name: "Food Drive", date: "09-10-2025" },
+            { id: 4, name: "Puppy Pickup", date: "10-17-2025"}
         ];
         setVolunteers(sampleVolunteers);
         setEvents(sampleEvents);
