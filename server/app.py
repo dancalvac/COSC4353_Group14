527c49a161804984c5af69b22d0d53ca08078c93
dancalvac <calvac.daniel@gmail.com>
2025-07-31 23:18:39 -0500

    Fixing deployment problem

--- server/app.py
+++ server/app.py
@@ -27,11 +27,6 @@ def create_app():
 # The global 'app' instance that Gunicorn will import
 app = create_app()
 
-#if __name__ == '__main__':
-    # Railway will provide the PORT environment variable
- #   port = int(os.environ.get('PORT', 5001)) # Default to 5000 for local dev
-    
-  #  app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
